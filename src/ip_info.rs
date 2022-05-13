e3b9db60fb035fc329098f43deccd5c42a9c6b5c
Eugen Rata <eugen.rata@gmail.com>
2022-05-12 21:30:44 -0500

    Update src/ip_info.rs
    Co-authored-by: Remoun Metyas <remoun.metyas@gmail.com>

--- src/ip_info.rs
+++ src/ip_info.rs
@@ -16,22 +16,6 @@ impl LocationProvider for IpInfoIoFetch {
       .headers(json_headers)
       .send()?
       .error_for_status()?;
-    let data = response.text()?;
-    let data_json: serde_json::Value = serde_json::from_str(&data)?;
-
-    let data_missing_err = Err(ConfigError::DataMissing(data_json.to_string()));
-    let country: &str = match data_json["country"].as_str() {
-      Some(c) => c,
-      None => return data_missing_err,
-    };
-    let region: &str = match data_json["region"].as_str() {
-      Some(r) => r,
-      None => return data_missing_err,
-    };
-
-    Ok(Location {
-      country_code: String::from(country),
-      region: String::from(region),
-    })
+    Ok(response.json()?)
   }
 }
