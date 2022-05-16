021ee30a4d51f80b1351c657ae8eaeee8cbe700a
Eugen Rata <eugen.rata@gmail.com>
2022-05-16 12:13:50 -0500

    Update src/ip_who.rs
    Co-authored-by: Remoun Metyas <remoun.metyas@gmail.com>

--- src/ip_who.rs
+++ src/ip_who.rs
@@ -20,21 +20,6 @@ impl LocationProvider for IpWhoIs {
       .get("https://ipwho.is/")
       .send()?
       .error_for_status()?;
-    let data = response.text()?;
-    let data_json: serde_json::Value = serde_json::from_str(&data)?;
-
-    let data_missing_err = Err(ConfigError::DataMissing(data_json.to_string()));
-    let country = data_json["country_code"]
-      .as_str()
-      .ok_or_else(|| ConfigError::DataMissing(data_json.to_string()))?;
-    let region: &str = match data_json["region_code"].as_str() {
-      Some(r) => r,
-      None => return data_missing_err,
-    };
-
-    Ok(Location {
-      country_code: String::from(country),
-      region: String::from(region),
-    })
+    Ok(response.json()?)
   }
 }
